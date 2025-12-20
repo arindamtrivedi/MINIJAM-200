225913f3dea48c3df6bac9e48593f238dd40fd7b
bryan <176309243+Yamaworld8@users.noreply.github.com>
2025-12-20 10:58:51 +0100

    Update player.gd

--- minijam_200/scripts/scenes/entities/player/player.gd
+++ minijam_200/scripts/scenes/entities/player/player.gd
@@ -9,6 +9,8 @@ const SPEED: float = 200.0
 const GRAVITY: float = 20.0
 const JUMP_VELOCITY: float = -400.0
 
+var can_jump: bool = false
+
 func _physics_process(delta: float) -> void:
 	if !is_on_floor():
 		velocity.y += GRAVITY
@@ -16,8 +18,9 @@ func _physics_process(delta: float) -> void:
 		velocity.y = 0.0
 	
 	# MOVEMENT
-	if is_on_floor() and Input.is_action_just_pressed("move_jump"):
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor() and can_jump:
+		if Input.is_action_just_pressed("move_jump"):
+			velocity.y = JUMP_VELOCITY
 	
 	var input_direction: float = Input.get_axis("move_left", "move_right")
 	
