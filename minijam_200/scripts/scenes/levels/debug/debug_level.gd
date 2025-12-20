02562693b622b33e122bc0d2a32d80edc351e498
bryan <176309243+Yamaworld8@users.noreply.github.com>
2025-12-20 10:55:20 +0100

    Create new debug dialogue

--- minijam_200/scripts/scenes/levels/debug/debug_level.gd
+++ minijam_200/scripts/scenes/levels/debug/debug_level.gd
@@ -2,5 +2,5 @@ extends Level
 
 
 func _ready() -> void:
-	var dialogue: DialogueData = preload("uid://5tj3rm660eyv")
+	var dialogue: DialogueData = preload("uid://c8j0l3816jaoi")
 	DialogueManager.play(dialogue);
